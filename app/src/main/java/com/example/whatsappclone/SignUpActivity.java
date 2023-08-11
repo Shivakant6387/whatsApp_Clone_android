69ab3cae403f1da1384dc5265cb7102c6219fa76
Shivakant6387 <shivakantsingh900@gmail.com>
2023-08-11 16:36:04 +0530

    firebase database connect and sign up bt successfully completed

--- app/src/main/java/com/example/whatsappclone/SignUpActivity.java
+++ app/src/main/java/com/example/whatsappclone/SignUpActivity.java
@@ -1,16 +1,35 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.whatsappclone.databinding.ActivitySignUpBinding;
+import com.example.whatsappclone.model.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    ActivitySignUpBinding binding;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        //setContentView(R.layout.activity_sign_up);
+        binding=ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mAuth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
         // Get a reference to the ActionBar
         ActionBar actionBar = getSupportActionBar();
 
@@ -19,5 +38,35 @@ public class SignUpActivity extends AppCompatActivity {
             // Now you can call methods on the ActionBar
             actionBar.hide();
         }
+        progressDialog=new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating  your account.");
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!binding.txtUsername.getText().toString().isEmpty()&& !binding.txtEmail.getText().toString().isEmpty()&& !binding.txtPassword.getText().toString().isEmpty()){
+                    progressDialog.show();
+                    mAuth.createUserWithEmailAndPassword( binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressDialog.dismiss();
+                                    if (task.isSuccessful()){
+                                        Users user=new Users(binding.txtUsername.getText().toString(),binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString());
+                                        String id =task.getResult().getUser().getUid();
+                                        database.getReference().child("users").child(id).setValue(user);
+                                        Toast.makeText(SignUpActivity.this,"Sign Up  Successful",Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Toast.makeText(SignUpActivity.this,task.getException().toString(),Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this,"Enter Credentials",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
