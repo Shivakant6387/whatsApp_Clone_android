3727b5cb50c517ce5dac492a48fe86b332f8efd4
Shivakant6387 <shivakantsingh900@gmail.com>
2023-08-11 13:21:10 +0530

    sign in

--- app/src/main/java/com/example/whatsappclone/SignInActivity.java
+++ app/src/main/java/com/example/whatsappclone/SignInActivity.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-        //problem null object
-//        getSupportActionBar().hide();
+        setContentView(R.layout.activity_sign_in);
+        //null object reference
+        //getSupportActionBar().hide();
     }
 }
