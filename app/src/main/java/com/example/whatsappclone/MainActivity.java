2c301cacc0b71ac098e978ddf5e981de0e8648a1
Shivakant6387 <shivakantsingh900@gmail.com>
2023-08-13 16:19:46 +0530

    logout and chat

--- app/src/main/java/com/example/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.whatsappclone.adapter.FragmentsAdapter;
 import com.example.whatsappclone.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         auth = FirebaseAuth.getInstance();
+        binding.viewPager.setAdapter(new FragmentsAdapter(getSupportFragmentManager()));
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
 
     }
 
@@ -28,5 +36,23 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return true;
 
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Toast.makeText(this, "setting is clicked", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.groupChat) {
+            Toast.makeText(this, "Group Chat is Started", Toast.LENGTH_SHORT).show();
+
+        } else if (item.getItemId() == R.id.logout) {
+            //Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+            auth.signOut();
+            Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+            startActivity(intent);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/whatsappclone/fragments/ChatsFragment.java
@@ -0,0 +1,71 @@
+package com.example.whatsappclone.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.example.whatsappclone.R;
+import com.example.whatsappclone.adapter.UsersAdapter;
+import com.example.whatsappclone.databinding.FragmentChatsBinding;
+import com.example.whatsappclone.model.Users;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class ChatsFragment extends Fragment {
+
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentChatsBinding binding;
+    ArrayList<Users> list = new ArrayList<>();
+    FirebaseDatabase database;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        binding = FragmentChatsBinding.inflate(inflater, container, false);
+        database = FirebaseDatabase.getInstance();
+        UsersAdapter adapter = new UsersAdapter(list, getContext());
+        binding.chatsRecycleView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        binding.chatsRecycleView.setLayoutManager(linearLayoutManager);
+        database.getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    users.setUserId(dataSnapshot.getKey());
+                    if (!users.getUserId().equals(FirebaseAuth.getInstance().getUid())){
+                        list.add(users);
+                    }
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return binding.getRoot();
+    }
+}
